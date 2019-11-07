345ca2ee559a2aa29625e66a2fcb184bc298b8a8
Hans Roh <hansroh@gmail.com>
2019-11-07 11:08:35 +0000

    linting try #5

--- tests/test_h3.py
+++ tests/test_h3.py
@@ -11,7 +11,13 @@ from aioquic.h3.connection import (
     StreamType,
     encode_frame,
 )
-from aioquic.h3.events import ConnectionShutdownInitiated, DataReceived, HeadersReceived, PushCanceled, PushPromiseReceived
+from aioquic.h3.events import (
+    ConnectionShutdownInitiated,
+    DataReceived,
+    HeadersReceived,
+    PushCanceled,
+    PushPromiseReceived
+)
 from aioquic.h3.exceptions import NoAvailablePushIDError
 from aioquic.quic.configuration import QuicConfiguration
 from aioquic.quic.events import StreamDataReceived
