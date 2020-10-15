e55ad620820bd873cec5b8fe770f58d7247e8820
Jeremy Lainé <jeremy.laine@m4x.org>
2020-10-15 09:47:44 +0200

    [package] mention Python 3.9 compatibility

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP",
     ],
     ext_modules=[
