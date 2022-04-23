64c90237dfb432ac595f50a3437b6aa019db3265
msoxzw <56633971+msoxzw@users.noreply.github.com>
2022-04-23 09:36:16 +0000

    Revert "[asyncio] optimize QUIC speed on Windows"
    This reverts commit ee1d4a0c8df56d7b5604a006d22055420cb39fa9.

--- src/aioquic/asyncio/protocol.py
+++ src/aioquic/asyncio/protocol.py
@@ -1,5 +1,4 @@
 import asyncio
-import time
 from typing import Any, Callable, Dict, Optional, Text, Tuple, Union, cast
 
 from ..quic import events
@@ -58,7 +57,7 @@ class QuicConnectionProtocol(asyncio.DatagramProtocol):
 
         This method can only be called for clients and a single time.
         """
-        self._quic.connect(addr, now=time.perf_counter())
+        self._quic.connect(addr, now=self._loop.time())
         self.transmit()
 
     async def create_stream(
@@ -100,7 +99,7 @@ class QuicConnectionProtocol(asyncio.DatagramProtocol):
         self._transmit_task = None
 
         # send datagrams
-        for data, addr in self._quic.datagrams_to_send(now=time.perf_counter()):
+        for data, addr in self._quic.datagrams_to_send(now=self._loop.time()):
             self._transport.sendto(data, addr)
 
         # re-arm timer
@@ -133,7 +132,7 @@ class QuicConnectionProtocol(asyncio.DatagramProtocol):
         self._transport = cast(asyncio.DatagramTransport, transport)
 
     def datagram_received(self, data: Union[bytes, Text], addr: NetworkAddress) -> None:
-        self._quic.receive_datagram(cast(bytes, data), addr, now=time.perf_counter())
+        self._quic.receive_datagram(cast(bytes, data), addr, now=self._loop.time())
         self._process_events()
         self.transmit()
 
@@ -170,7 +169,7 @@ class QuicConnectionProtocol(asyncio.DatagramProtocol):
         return reader, writer
 
     def _handle_timer(self) -> None:
-        now = max(self._timer_at, time.perf_counter())
+        now = max(self._timer_at, self._loop.time())
         self._timer = None
         self._timer_at = None
         self._quic.handle_timer(now=now)
