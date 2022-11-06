9dc3cf94ad878fd85b43a9ba8901e64669cec2f4
Maximilian Hils <git@maximilianhils.com>
2022-11-06 22:11:48 +0100

    build abi3 wheels
     - change c extensions to use the limited ABI
     - update `setup.py` to build abi3 wheels
     - add Python 3.11 to build matrix
     - rebased 2023may08
    
    Co-authored-by: Kelsey Gilbert <kelsey.gilbert@mozilla.com>
    Co-authored-by: Jeremy Lainé <jeremy.laine@m4x.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 
 import setuptools
+from wheel.bdist_wheel import bdist_wheel
 
 if sys.platform == "win32":
     extra_compile_args = []
@@ -9,18 +10,34 @@ else:
     extra_compile_args = ["-std=c99"]
     libraries = ["crypto"]
 
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            return "cp37", "abi3", plat
+
+        return python, abi, plat
+
+
 setuptools.setup(
     ext_modules=[
         setuptools.Extension(
             "aioquic._buffer",
             extra_compile_args=extra_compile_args,
             sources=["src/aioquic/_buffer.c"],
+            define_macros=[("Py_LIMITED_API", "0x03070000")],
+            py_limited_api=True,
         ),
         setuptools.Extension(
             "aioquic._crypto",
             extra_compile_args=extra_compile_args,
             libraries=libraries,
             sources=["src/aioquic/_crypto.c"],
+            define_macros=[("Py_LIMITED_API", "0x03070000")],
+            py_limited_api=True,
         ),
     ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
