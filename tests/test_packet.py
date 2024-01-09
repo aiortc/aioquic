1db3fa7e8cbd4f28b0e7dd58a4b130c4065e7cc6
Jeremy Lainé <jeremy.laine@m4x.org>
2024-01-09 05:45:39 +0100

    Clean up retry packet test (fixes: #447)

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -112,15 +112,12 @@ class PacketTest(TestCase):
         self.assertEqual(buf.tell(), 125)
 
         # check integrity
-        if False:
-            self.assertEqual(
-                get_retry_integrity_tag(
-                    buf.data_slice(0, 109),
-                    original_destination_cid,
-                    version=header.version,
-                ),
-                header.integrity_tag,
-            )
+        self.assertEqual(
+            get_retry_integrity_tag(
+                buf.data_slice(0, 109), original_destination_cid, version=header.version
+            ),
+            header.integrity_tag,
+        )
 
         # serialize
         encoded = encode_quic_retry(
@@ -130,8 +127,6 @@ class PacketTest(TestCase):
             original_destination_cid=original_destination_cid,
             retry_token=header.token,
         )
-        with open("bob.bin", "wb") as fp:
-            fp.write(encoded)
         self.assertEqual(encoded, data)
 
     def test_pull_retry_draft_29(self):
